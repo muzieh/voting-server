7aed8de62f5146b6d91969fd1cb45b4e77ce642c
Marcin Owerczuk <muzieh@gmail.com>
2016-07-07 08:18:36 +0100

    some more chai exploring tests

--- test/test_chai.js
+++ test/test_chai.js
@@ -21,4 +21,31 @@ describe('test on chai', () => {
 			expect(a).to.have.length(2);
 		});
 	});
+
+	describe('checking type', () => {
+		it('5 is number', () => {
+			const num = 4;
+			expect(num).to.be.a('number');
+		});
+
+		it('function a function', () => {
+			var f = (a) => { return a; };
+			expect(f).to.be.a('function');
+		});
+
+		it('date is a function', () => {
+			var d = new Date();
+			expect(d).to.be.a('date');
+		});
+
+		it('null is a null', () => {
+			var d = null;
+			expect(d).to.be.a('null');
+		});
+
+		it('undefined is a undefined', () => {
+			expect(undefined).to.be.a('undefined');
+		});
+	});
+
 });
