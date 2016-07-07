f1d31c7dfd23b9057b0f8a3485abd2f5c808d097
Marcin Owerczuk <muzieh@gmail.com>
2016-07-07 09:12:03 +0100

    voting for a pair

--- test/core_spec.js
+++ test/core_spec.js
@@ -1,7 +1,7 @@
 import {List, Map, fromJS} from 'immutable';
 import {expect} from 'chai';
 
-import {setEntries, next} from '../src/core';
+import {setEntries, next, vote} from '../src/core';
 
 describe('application logic', () => {
 
@@ -30,10 +30,12 @@ describe('application logic', () => {
 	describe('next pair', () => {
 
 		it('get next pair', () => {
-			const state = fromJS({entries: ['Trainspotting', '28 days later', 'Sunshine']})
+			const state = fromJS({entries: ['Trainspotting', '28 days later', 'Sunshine']});
 			const nextState = next(state);
 			expect(nextState).to.equal(Map({
-				vote: Map({ pair: List.of('Trainspotting', '28 days later')}),
+				vote: Map({
+					pair: List.of('Trainspotting', '28 days later')
+				}),
 				entries: List.of('Sunshine')
 			}));
 		});
@@ -41,5 +43,46 @@ describe('application logic', () => {
 
 	describe('vote', () => {
 
+		it('creates a tally for the voted entry', () => {
+			const state = fromJS({
+				vote: {
+						pair: ['Trainspotting', '28 days later']
+				},
+				entries: List()});
+			const nextState = vote(state, 'Trainspotting');
+			expect(nextState).to.equal(Map({
+				vote: Map({
+					pair: List.of('Trainspotting', '28 days later'),
+					tally: Map({
+						'Trainspotting': 1
+					})
+				}),
+				entries: List()
+			}));
+		});
+
+		it('adds to existing tally for the voted entry', () => {
+			const state = fromJS({
+				vote: {
+						pair: ['Trainspotting', '28 days later'],
+						tally: {
+							'Trainspotting': 4,
+							'28 days later': 3
+						}
+				},
+				entries: List()});
+			const nextState = vote(state, '28 days later');
+			expect(nextState).to.equal(Map({
+				vote: Map({
+					pair: List.of('Trainspotting', '28 days later'),
+					tally: Map({
+						'Trainspotting': 4,
+						'28 days later': 4
+					})
+				}),
+				entries: List()
+			}));
+			
+		});
 	});
 });
